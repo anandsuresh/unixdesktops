02b9045a0b6f04a5c3823ee5fa57946d1e2230ca
Rose Dovell <rosedovell@protonmail.com>
2016-12-30 16:10:59 -0500

    GNOME 3

---
+++ install-kde4-on-freebsd-11.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+pkg install -y xorg kde
+
+if grep "dbus_enable" /etc/rc.conf > /dev/null
+then
+  echo "Skipping enabling DBUS"
+else
+  echo 'dbus_enable="YES"' >> /etc/rc.conf
+fi
+
+if grep "hald_enable" /etc/rc.conf > /dev/null
+then
+  echo "Skipping enabling HALD"
+else
+  echo 'hald_enable="YES"' >> /etc/rc.conf
+fi
+
+if grep "kdm4_enable" /etc/rc.conf > /dev/null
+then
+  echo "Skipping enabling KDM4"
+else
+  echo 'kdm4_enable="YES"' >> /etc/rc.conf
+fi
