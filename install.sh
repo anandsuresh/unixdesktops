993a1562cb3f6e448c978070ecd3382c29a3daa9
Rose Dovell <rosedovell@protonmail.com>
2017-01-02 21:47:18 -0500

    refactor

--- install.sh
+++ install.sh
@@ -1,70 +1,56 @@
 #!/bin/sh
-
 if [ "$(id -u)" -ne 0 ]
 then
   echo "Please run as root"
   exit
 fi
 
-echo "===================================================="
-echo "|Step 1: Choose an operating system(enter a number)|"
-echo "===================================================="
-echo "|"
-echo "|1: FreeBSD 11"
-echo "|"
-printf "|Your choice >"
-read -r os_choice
-echo "|"
+echo "===================================="
+echo "|Step 1: Choose an operating system|"
+echo "===================================="
+find scripts -maxdepth 1  | cut -d '/' -f 2 | grep '^[0-9]\|[a-z]' | grep -v scripts | sort | column -x -c 80
+echo ""
+printf "Your choice >"
+read -r os
 
-if [ "$os_choice" -eq "1" ]
-then
-  os="FreeBSD-11"
-fi
+echo "================================"
+echo "|Choose a CPU architecture type|"
+echo "================================"
+find scripts/$os -maxdepth 1 | cut -d '/' -f 3 | grep '^[0-9]\|[a-z]' | sort | column -x -c 80
+echo ""
+printf "Your choice >"
+read -r arch
 
-echo "================================================"
-echo "|Choose a CPU architecture type(enter a number)|"
-echo "================================================"
-echo "|"
-echo "|1: amd64"
-echo "|"
-printf "|Your choice >"
-read -r arch_choice
-echo "|"
-
-if [ "$arch_choice" -eq "1" ]
-then
-  arch="amd64"
-fi
-
-echo "============================================="
-echo "|Choose a desktop environment(enter a name)|"
-echo "============================================="
-find scripts/install-*-on-$os-$arch.sh | cut -d '-' -f 2 | column -x -c 80
-printf "|Your choice >"
+echo "=============================="
+echo "|Choose a desktop environment|"
+echo "=============================="
+find scripts/$os/$arch -maxdepth 1 | cut -d '/' -f 4  | egrep ^[0-9]\|[a-z] | grep -v base | grep -v xdm| grep -v keylaunch| sort | column -x -c 80
+echo ""
+printf "Your choice >"
 read -r de
 
 echo "========="
 echo "|WARNING|"
 echo "========="
-echo "|"
-echo "|This will attempt to install $de on $os $arch, are you sure you want to continue? (y/n)"
-echo "|"
-printf "|Your choice >"
+echo ""
+echo "This will attempt to install $de on $os $arch, are you sure you want to continue? (yes/no)"
+echo ""
+printf "Your choice >"
 read -r warning_choice
-if [ "$warning_choice" != "y" ]
+if [ "$warning_choice" != "yes" ]
 then
   exit 0
 fi
 
-sh scripts/install-"$de"-on-"$os"-"$arch".sh
+sh scripts/"$os/$arch/$de"
 
-echo "================================="
-echo "|Would you like to reboot? (y/n)|"
-echo "================================="
-echo "|"
-printf "|Your choice >"
+echo "===================================="
+echo "|Would you like to reboot? (yes/no)|"
+echo "===================================="
+echo ""
+printf "Your choice >"
 read -r reboot_choice
-if [ "$reboot_choice" = "y" ]
+if [ "$reboot_choice" = "yes" ]
 then
     reboot
 fi
